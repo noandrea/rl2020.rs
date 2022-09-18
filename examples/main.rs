aa3b2f099c6faa9f2ad45618cc3b0d4ae6bc0f4e
Andrea Giacobino <no.andrea@gmail.com>
2022-09-18 11:21:18 +0200

    chore(example): improve example output and docs

--- examples/main.rs
+++ examples/main.rs
@@ -1,6 +1,67 @@
 use rl2020::{CredentialStatus, RevocationList2020};
 use serde_derive::{Deserialize, Serialize};
 
+fn main() {
+    println!("Hello, RevocationList2020!");
+
+    // create a new revocation list
+    let mut rl = RevocationList2020::new("https://example.com/credentials/status/3", 16).unwrap();
+    println!("{}", rl);
+
+    // create a credential that uses the revocation list
+    let c = create_credential();
+    let c_idx = c.credential_status.revocation_list_index;
+
+    // check if the credential is revoked
+    let revoked = rl.is_revoked(&c).unwrap();
+    println!("credential at index {} is revoked? {}", c_idx, revoked);
+    // revoke the credential
+    println!("revoking credential at index {}", c_idx);
+    rl.revoke(&c).unwrap();
+    // print the updated revocation list
+    println!("{}", rl);
+    //check if the credential is revoked
+    let revoked = rl.is_revoked(&c).unwrap();
+    println!("credential at index {} is revoked? {}", c_idx, revoked);
+    // reset the credential revocation
+    println!("resetting status for credential at index {}", c_idx);
+    rl.reset(&c).unwrap();
+    // print the updated revocation list
+    println!("{}", rl);
+    //check if the credential is revoked
+    let revoked = rl.is_revoked(&c).unwrap();
+    println!("credential at index {} is revoked? {}", c_idx, revoked);
+}
+
+fn create_credential() -> VerifableCredential {
+    // Some JSON input data as a &str. Maybe this comes from the user.
+    let data = r#"
+    {
+        "@context": [
+            "https://www.w3.org/2018/credentials/v1",
+            "https://w3id.org/vc-revocation-list-2020/v1"
+        ],
+        "id": "https://example.com/credentials/23894672394",
+        "type": [
+            "VerifiableCredential"
+        ],
+        "issuer": "did:example:12345",
+        "issued": "2020-04-05T14:27:42Z",
+        "credentialStatus": {
+            "id": "https://dmv.example.gov/credentials/status/3#7812",
+            "type": "RevocationList2020Status",
+            "revocationListIndex": 7812,
+            "revocationListCredential": "https://example.com/credentials/status/3"
+        },
+        "credentialSubject": {
+            "id": "did:example:abcdefg",
+            "type": "Person"
+        },
+        "proof": {}
+    }"#;
+    serde_json::from_str::<VerifableCredential>(data).unwrap()
+}
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct VerifableCredential {
     #[serde(rename = "credentialStatus")]
@@ -35,77 +96,4 @@ impl CredentialStatus for VerifableCredential {
             self.credential_status.typ.to_owned(),
         )
     }
-}
-
-fn main() {
-    println!("Hello, RevocationList2020!");
-
-    // create a new revocation list
-    let mut rl = RevocationList2020::new("https://example.com/credentials/status/3", 16).unwrap();
-    println!("{}", rl);
-
-    // create a credential that uses the revocation list
-    let c = create_credential();
-
-    let revoked = rl.is_revoked(&c).unwrap();
-    println!(
-        "credential with id {} is revoked? {}",
-        c.credential_status.revocation_list_index, revoked
-    ); // prints false
-
-    println!(
-        "revoking credential with index {}",
-        c.credential_status.revocation_list_index
-    );
-    rl.revoke(&c).unwrap();
-    let revoked = rl.is_revoked(&c).unwrap();
-    println!("{}", rl);
-
-    println!(
-        "credential with id {} is revoked? {}",
-        c.credential_status.revocation_list_index, revoked
-    ); // prints true
-
-    println!(
-        "resetting status for credential with index {}",
-        c.credential_status.revocation_list_index
-    );
-
-    rl.reset(&c).unwrap();
-    let revoked = rl.is_revoked(&c).unwrap();
-    println!("{}", rl);
-
-    println!(
-        "credential with id {} is revoked? {}",
-        c.credential_status.revocation_list_index, revoked
-    ); // prints false
-}
-
-fn create_credential() -> VerifableCredential {
-    // Some JSON input data as a &str. Maybe this comes from the user.
-    let data = r#"
-    {
-        "@context": [
-            "https://www.w3.org/2018/credentials/v1",
-            "https://w3id.org/vc-revocation-list-2020/v1"
-        ],
-        "id": "https://example.com/credentials/23894672394",
-        "type": [
-            "VerifiableCredential"
-        ],
-        "issuer": "did:example:12345",
-        "issued": "2020-04-05T14:27:42Z",
-        "credentialStatus": {
-            "id": "https://dmv.example.gov/credentials/status/3#7812",
-            "type": "RevocationList2020Status",
-            "revocationListIndex": 7812,
-            "revocationListCredential": "https://example.com/credentials/status/3"
-        },
-        "credentialSubject": {
-            "id": "did:example:abcdefg",
-            "type": "Person"
-        },
-        "proof": {}
-    }"#;
-    serde_json::from_str::<VerifableCredential>(data).unwrap()
 }
